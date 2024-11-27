7790a0bcac3cd031789407cd009792a4c787c135
gnoni17 <gnonigabriele@gmail.com>
2024-11-27 16:29:17 +0100

    fix: proprietà react

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -112,9 +112,9 @@ export function ContactForm() {
           {requestStatus === 'success' && (
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
           )}
